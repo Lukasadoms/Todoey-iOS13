f67399a2a7fe6af4766aa92e92a46bf336be741b
Lukas Adomavičius <73688674+Lukasadoms@users.noreply.github.com>
2021-01-18 18:12:59 +0200

    implement CoreData v1

---
+++ Todoey/Controllers/ToDoListViewController.swift
@@ -0,0 +1,104 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Philipp Muellauer on 02/12/2019.
+//  Copyright © 2019 App Brewery. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class ToDoListViewController: UITableViewController {
+    
+    var itemArray = [Item]()
+    
+    let defaults = UserDefaults.standard
+    
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print(FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist"))
+       // loadData()
+    }
+    
+    // MARK: - TableView Datasource Methods
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        
+        cell.textLabel!.text = itemArray[indexPath.row].title
+        cell.accessoryType = itemArray[indexPath.row].done ? .checkmark : .none
+        
+        return cell
+    }
+    
+    // MARK: - TableView Delegate Methods
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+              
+        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
+        
+        saveData()
+    }
+    
+    // MARK: - Add new Items functionality
+    
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
+        var todoey = UITextField()
+        
+        let alert = UIAlertController(title: "Add New Todoey Item", message: "", preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
+            
+            
+            
+            let newItem = Item(context: self.context)
+            newItem.title = todoey.text!
+            newItem.done = false
+            self.itemArray.append(newItem)
+            
+            self.saveData()
+        }
+        
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Create new item"
+            todoey = alertTextField
+        }
+        
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
+}
+
+extension ToDoListViewController {
+    func saveData() {
+        
+        do {
+            try context.save()
+        } catch {
+            print("Error saving context \(error)")
+        }
+        self.tableView.reloadData()
+    }
+    
+//    func loadData() {
+//        if let data = try? Data(contentsOf: dataFilePath!) {
+//            let decoder = PropertyListDecoder()
+//            do {
+//            itemArray = try decoder.decode([Item].self, from: data)
+//            } catch {
+//                print("Error decoding data, \(error)")
+//            }
+//        }
+//    }
+}
+
+
+
+
